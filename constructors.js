060d1e011566019081ab17205ffd43dc51d461c8
Joe Cheney <joerustedorange@gmail.com>
2020-11-12 20:24:17 -0700

    updating

--- constructors.js
+++ constructors.js
@@ -16,6 +16,11 @@
 
 // Code here
 
+function CarFactory(make, model) {
+  this.make = make;
+  this.model = model;
+}
+
 ////////// PROBLEM 2 //////////
 
 // Do not edit the code below.
@@ -34,6 +39,8 @@ function Employee(name, email, hireDate) {
   */
   
   // Code here
+  
+  const bob = new Employee('Bob', 'bob@gmail.com', '01-02-98')
   
   ////////// PROBLEM 3 //////////
   
@@ -57,6 +64,8 @@ function Employee(name, email, hireDate) {
   */
   
   // Code here
+  
+
   
   ////////// PROBLEM 4 //////////
   
